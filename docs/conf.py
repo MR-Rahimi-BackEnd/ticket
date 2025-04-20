2805ddbd423d2fc3c7de8a815cc7ddb4d93ca37f
Mohammadreza <mrrahimi357@gmail.com>
2025-04-20 09:26:18 +0330

    test docstring

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ release = '0.1.0'
 
 
 
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../ticket'))
 
 
 # -- General configuration ---------------------------------------------------
